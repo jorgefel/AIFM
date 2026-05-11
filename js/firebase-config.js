6070d424389974d06e922d0155f8eb1ab10808b3
jorgefel <167446402+jorgefel@users.noreply.github.com>
2026-05-11 13:22:45 -0500

    correccion de firebase

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,9 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDpo89PWGkDFQ_WqKLO8IoWhJQZpLh83lA",
-  authDomain: "airbnb-finanzas.firebaseapp.com",
-  databaseURL: "https://airbnb-finanzas-default-rtdb.firebaseio.com",
-  projectId: "airbnb-finanzas",
-  storageBucket: "airbnb-finanzas.firebasestorage.app",
-  messagingSenderId: "777842025336",
-  appId: "1:777842025336:web:3e417ad09b261879161e8d",
-  measurementId: "G-5SCKNE3QZY"
+  apiKey: "AIzaSy...",
+  authDomain: "tu-proyecto.firebaseapp.com",
+  databaseURL: "https://tu-proyecto-default-rtdb.firebaseio.com",
+  projectId: "tu-proyecto",
+  storageBucket: "tu-proyecto.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abcdef"
 };
